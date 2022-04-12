a4fc62d9dad26313cbd9648ebe845e58789a07eb
Eduardo Debortoli Piccoli <edudebortoli@icloud.com>
2022-04-12 14:32:15 -0300

    Controller refactor
    Banco não crasha mais no update de ID sem referencia

--- app/Controllers/Http/EstadosController.ts
+++ app/Controllers/Http/EstadosController.ts
@@ -7,43 +7,28 @@ export default class EstadosController {
   public async index() {
     // const estados = await Estado.all()
     const estados = await Database.table('estados')
-    return {
-      message: 'Estados listados',
-      estados: estados,
-    }
+    return estados
   }
   public async store({ request, response }: HttpContextContract) {
-    const body = request.body()
+    const body = request.only(['nom_estado'])
     const estado = await Estado.create(body)
     response.status(201)
-    return {
-      message: 'Estado Criado',
-      estado: estado,
-    }
+    return estado
   }
   public async show({ params }: HttpContextContract) {
     const estado = await Estado.findOrFail(params.id)
-
-    return {
-      estado: estado,
-    }
+    return estado
+  }
+  public async update({ params, request }: HttpContextContract) {
+    const estado = await Estado.findOrFail(params.id)
+    const body = request.only(['nom_estado'])
+    estado.merge(body)
+    await estado.save()
+    return estado
   }
   public async destroy({ params }: HttpContextContract) {
     const estado = await Estado.findOrFail(params.id)
     estado.delete()
-    return {
-      message: 'estado deletado',
-      estado: estado,
-    }
-  }
-  public async update({ params, request }: HttpContextContract) {
-    const body = request.body()
-    const estado = await Estado.findOrFail(params.id)
-    estado.nom_estado = body.nom_estado
-    estado.save()
-    return {
-      message: 'estado alterado',
-      estado: estado,
-    }
+    return estado
   }
 }
