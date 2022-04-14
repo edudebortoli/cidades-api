4c2cb239bbcc930fb1421f6c87672b868c89009f
Eduardo Debortoli Piccoli <edudebortoli@icloud.com>
2022-04-14 08:54:58 -0300

    Update CidadesController.ts

--- app/Controllers/Http/CidadesController.ts
+++ app/Controllers/Http/CidadesController.ts
@@ -1,36 +1,36 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Database from '@ioc:Adonis/Lucid/Database'
 import Cidade from 'App/Models/Cidade'
 
 export default class CidadesController {
   public async index() {
-    // const cidades = await Cidade.all()
-    const cidades = await Database.query()
-      .select(
-        'cidades.id_cidade',
-        'cidades.nom_cidade',
-        'cidades.id_ibge',
-        'estados.id_estado',
-        'estados.nom_estado',
-        'paises.id_pais',
-        'paises.nom_pais',
-        'cidades.created_at',
-        'cidades.updated_at'
-      )
-      .from('cidades')
-      .innerJoin('estados', 'cidades.id_estado', 'estados.id_estado')
-      .innerJoin('paises', 'cidades.id_pais', 'paises.id_pais')
+    const cidades = await Cidade.query().preload('estado').preload('pais')
+    // .whereRaw("nom_cidade ilike '%flores%'")
+    // const cidades = await Database.query()
+    //   .select(
+    //     'cidades.id_cidade',
+    //     'cidades.nom_cidade',
+    //     'cidades.id_ibge',
+    //     'estados.id_estado',
+    //     'estados.nom_estado',
+    //     'paises.id_pais',
+    //     'paises.nom_pais',
+    //     'cidades.created_at',
+    //     'cidades.updated_at'
+    //   )
+    //   .from('cidades')
+    //   .innerJoin('estados', 'cidades.id_estado', 'estados.id_estado')
+    //   .innerJoin('paises', 'cidades.id_pais', 'paises.id_pais')
+
     return cidades
   }
   public async store({ request, response }: HttpContextContract) {
     const body = request.only(['nom_cidade', 'id_ibge', 'id_estado', 'id_pais'])
     const cidade = await Cidade.create(body)
-    response.status(201)
-    return cidade
+    return response.ok(cidade)
   }
-  public async show({ params }: HttpContextContract) {
+  public async show({ params, response }: HttpContextContract) {
     const cidade = await Cidade.findOrFail(params.id)
-    return cidade
+    return response.ok(cidade)
   }
   public async update({ params, request }: HttpContextContract) {
     const cidade = await Cidade.findOrFail(params.id)
